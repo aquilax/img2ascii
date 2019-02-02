d49375792ca360ce503e32322c5cf8ab6f54b2fb
aquilax <aquilax@gmail.com>
2019-02-02 14:30:17 +0100

    Cap width to 120px and add font-ratio

--- cmd/img2ascii.go
+++ cmd/img2ascii.go
@@ -7,11 +7,16 @@ import (
 	"os"
 
 	"github.com/aquilax/img2ascii"
+	"github.com/nfnt/resize"
 )
 
-func main() {
+// https://unix.stackexchange.com/questions/148569/standard-terminal-font-aspect-ratio
+const fontRatio = 1 / 2.5
+const maxWidth = 120
 
-	file, err := os.Open(os.Args[1])
+func main() {
+	fileName := os.Args[1]
+	file, err := os.Open(fileName)
 	if err != nil {
 		panic(err)
 	}
@@ -20,8 +25,16 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	bounds := img.Bounds()
+	width := uint(maxWidth)
+	if bounds.Max.X < maxWidth {
+		width = uint(bounds.Max.X)
+	}
+	height := uint(float64(bounds.Max.Y) * fontRatio * float64(width) / float64(bounds.Max.X))
+
+	newImage := resize.Resize(width, height, img, resize.Lanczos3)
 
 	c := img2ascii.AsciiNoColor{}
-	img2ascii.Process(img, os.Stdout, c)
+	img2ascii.Process(newImage, os.Stdout, c)
 
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module github.com/aquilax/img2ascii
 
-require golang.org/x/image v0.0.0-20190118043309-183bebdce1b2
+require (
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+	golang.org/x/image v0.0.0-20190118043309-183bebdce1b2
+)
